5b1af99b6c9f6c1c762161c4063ba185ac538166
mlema4 <mlema4@uic.edu>
2017-11-08 16:56:17 +0000

    actual final commit for project 7

--- project7/sw.js
+++ project7/sw.js
@@ -42,34 +42,6 @@ self.addEventListener('activate',  event => {
   event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request, {ignoreSearch:true}).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-
-
-// //importScripts('/cache-polyfill.js');
-// self.addEventListener('install', function(e) {
-// e.waitUntil(
-// caches.open('airhorner').then(function(cache) {
-// return cache.addAll([
-// '/',
-// '/index.html',
-// '/index.html?homescreen=1',
-// '/?homescreen=1',
-// '/styles/main.css',
-// '/scripts/main.min.js',
-// '/sounds/airhorn.mp3'
-// ]);
-// })
-// );
-// });
-
-
 self.addEventListener('fetch', function(event) {
 console.log(event.request.url);
 event.respondWith(
@@ -78,3 +50,13 @@ return response || fetch(event.request);
 })
 );
 });
+
+
+/*
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});*/

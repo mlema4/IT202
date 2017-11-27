4cf8fab94ce88089dc77496b1493bc913f480c74
mlema4 <mlema4@uic.edu>
2017-11-27 16:55:59 +0000

    working on map

--- final/index.js
+++ final/index.js
@@ -1,12 +1,13 @@
   $(document).ready(function() {
     $("#screen1").show();
+    
 
 
     $("#search").on("click", function() {
       $("#screen1").hide();
       $("#screen2").show();
       $("#search").prop("disabled", false);
-    
+
       var url = "https://data.cityofchicago.org/resource/cm53-g3up.json";
 
       parameters = new Object();
@@ -19,93 +20,97 @@
 
       var parameterString = $.param(parameters);
       console.log(parameterString);
-      
-      if(document.getElementById("pg").checked){
+
+      if (document.getElementById("pg").checked) {
         parameters.rating = "PG";
       }
       var parameterString = $.param(parameters);
-      var response1 = $.get (url + "?" + parameterString, function(){});
+      var response1 = $.get(url + "?" + parameterString, function() {});
 
 
-      if(document.getElementById("pg-13").checked){
+      if (document.getElementById("pg-13").checked) {
         parameters.rating = "PG-13";
       }
       var parameterString = $.param(parameters);
-      var response2 = $.get (url + "?" + parameterString, function(){});
-      
-      
-      if(document.getElementById("g").checked){
+      var response2 = $.get(url + "?" + parameterString, function() {});
+
+
+      if (document.getElementById("g").checked) {
         parameters.rating = "G";
       }
       var parameterString = $.param(parameters);
-      var response3 = $.get (url + "?" + parameterString, function(){});
+      var response3 = $.get(url + "?" + parameterString, function() {});
 
-      
-      if(document.getElementById("nr").checked){
+
+      if (document.getElementById("nr").checked) {
         parameters.rating = "NR";
       }
       var parameterString = $.param(parameters);
-      var response4 = $.get (url + "?" + parameterString, function(){});
-      
-    
-      $.get(url + "?" + parameterString, function(response) {
-        console.log(response);
-        console.log(url + "?" + parameterString);
-        console.log(response[0].location);
-         if (response[0].location){
-            console.log(response[0].location.coordinates[0])
-         }
-      $("#screen2").append("Results found: " + response.length);
-      var chicago = { lat:41.8781, lng: 87.6298};
-      var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 9,
-        center: chicago
-      });
-        map.addListener(map, 'idle', function() {
-        map.trigger(map, 'resize');
-});
-        
-        $.each(response, function(indx, val) {
-          console.log(val);
-          var template = $("#template").clone().removeAttr('id').removeClass("hidden");
-          
-          
-          $(".card-title", template).html("<Strong>" + val.dba_name + "</Strong>");
-          $(".card-subtitle", template).text(val.aka_name + " " + val.address);
-          $(".card-text", template).html("Inspection ID:" + val.inspection_id + "<br>" + "Inspection date: "+ val.inspection_date + "<br>" + "Inspection result: " + val.results);
-          $("#screen2").append(template);
-        //  $("#template", template).attr('id', "show");
-          
-          
-          var infoString = val.aka_name + "<br> Inspection Id: " + val.inspection_id + "<br> Address: " + val.address + "<br> Inspection Date: " + val.inspection_date + "<br> Inspection Result:  " + val.results;
-           var contentString = '<div id="content">'+
-            '<div id="siteNotice">'+
-            '</div>'+
-            '<h1 id="firstHeading" class="firstHeading">' + val.dba_name + '</h1>'+
-            '<div id="bodyContent">'+
-            '<p>' +
-            infoString + 
-            '</div>'+
-            '</div>';
-  
-  
-        var infowindow = new google.maps.InfoWindow({
-          content: contentString
-        });
-        
-          var latitude = parseFloat(val.latitude);
-          var longitude = parseFloat(val.longitude);
+      var response4 = $.get(url + "?" + parameterString, function() {});
 
-          var location = { lat: latitude, lng: longitude };
-          var marker = new google.maps.Marker({
-            position: location,
-            map: map,
-            title: ''
-          });
-          
-          marker.addListener('click', function(){
-            infowindow.open(map, marker);
-          })
+
+      $.get(url + "?" + parameterString, function(response) {
+        
+        /*        console.log(response);
+                console.log(url + "?" + parameterString);
+                console.log(response[0].location);
+                 if (response[0].location){
+                    console.log(response[0].location.coordinates[0])
+                 }*/
+
+        // $("#screen2").append("Results found: " + response.length);
+        var chicago = { lat: 41.8781, lng: 87.6298 };
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 9,
+          center: chicago
+        });
+        map.addListener(map, 'idle', function() {
+          map.trigger(map, 'resize');
+        });
+
+        $.each(response, function(indx, val) {
+          //console.log(val);
+          /*          var template = $("#template").clone().removeAttr('id').removeClass("hidden");
+                    
+                    
+                    $(".card-title", template).html("<Strong>" + val.dba_name + "</Strong>");
+                    $(".card-subtitle", template).text(val.aka_name + " " + val.address);
+                    $(".card-text", template).html("Inspection ID:" + val.inspection_id + "<br>" + "Inspection date: "+ val.inspection_date + "<br>" + "Inspection result: " + val.results);
+                    $("#screen2").append(template);
+                    //$("#template", template).attr('id', "show");*/
+
+          if (val.location) {
+            var infoString = val.title + "<br> : Date " + val.date + "<br> Address: " + val.location_address + "<br> Rating " + val.rating + "<br> Park:" + val.park;
+            var contentString = '<div id="content">' +
+              '<div id="siteNotice">' +
+              '</div>' +
+              '<h1 id="firstHeading" class="firstHeading">' + val.title + '</h1>' +
+              '<div id="bodyContent">' +
+              '<p>' +
+              infoString +
+              '</div>' +
+              '</div>';
+
+
+
+            var infowindow = new google.maps.InfoWindow({
+              content: contentString
+            });
+
+            var latitude = parseFloat(val.location.coordinates[0]);
+            var longitude = parseFloat(val.location.coordinates[1]);
+
+            var location = { lat: latitude, lng: longitude };
+            var marker = new google.maps.Marker({
+              position: location,
+              map: map,
+              title: ''
+            });
+
+            marker.addListener('click', function() {
+              infowindow.open(map, marker);
+            });
+          }
         });
 
       });
@@ -124,4 +129,4 @@
 
     });
   });
-
+  
